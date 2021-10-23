c86f22234958a4bf761ca82f00132479923239c7
Bweilandt <bweily643@gmail.com>
2021-10-22 21:00:09 -0500

    completed ex2e1 UnitTests

--- bweilandt2e1/Form1.cs
+++ bweilandt2e1/Form1.cs
@@ -38,11 +38,11 @@ namespace bweilandt2e1
 
             // 01
             decimal subtotal = Convert.ToDecimal(input01ATextBox.Text);
-            result01TextBox.Text = (subtotal >= 250 && subtotal < 500).ToString();
+            result01TextBox.Text = (LogicalOperations.q01(subtotal)).ToString();
 
             // 02
             int timeInService = Convert.ToInt32(input02ATextBox.Text);
-            result02TextBox.Text = (timeInService <= 4 || timeInService >= 12).ToString();
+            result02TextBox.Text = (LogicalOperations.q02(timeInService)).ToString();
 
             // 03 - 06 initialization
             bool isValid = Convert.ToBoolean(input03ATextBox.Text);
@@ -50,22 +50,22 @@ namespace bweilandt2e1
 
             // 03
             int counter = Convert.ToInt32(input03BTextBox.Text);
-            result03TextBox.Text = (isValid == true && counter++ < years).ToString();
+            result03TextBox.Text = (LogicalOperations.q03(isValid, years, counter)).ToString();
             SideEffect03TextBox.Text = counter.ToString();
 
             // 04
             counter = Convert.ToInt32(input03BTextBox.Text);
-            result04TextBox.Text = (isValid == true & counter++ < years).ToString();
+            result04TextBox.Text = (LogicalOperations.q04(isValid, years, counter)).ToString();
             SideEffect04TextBox.Text = counter.ToString();
 
             // 05
             counter = Convert.ToInt32(input03BTextBox.Text);
-            result05TextBox.Text = (isValid == true || counter++ < years).ToString();
+            result05TextBox.Text = (LogicalOperations.q05(isValid, years, counter)).ToString();
             SideEffect05TextBox.Text = counter.ToString();
 
             // 06
             counter = Convert.ToInt32(input03BTextBox.Text);
-            result06TextBox.Text = (isValid == true | counter++ < years).ToString();
+            result06TextBox.Text = (LogicalOperations.q06(isValid, years, counter)).ToString();
             SideEffect06TextBox.Text = counter.ToString();
 
             //07
@@ -74,7 +74,7 @@ namespace bweilandt2e1
             DateTime date = Convert.ToDateTime(input07CTextBox.Text);
             isValid = Convert.ToBoolean(input07DTextBox.Text);
             result07TextBox.Text = (
-            (date > startDate && date < expirationDate || isValid == true).ToString());
+            (LogicalOperations.q07(startDate, expirationDate, date, isValid)).ToString());
 
             //08
             decimal thisYTD = Convert.ToDecimal(input08ATextBox.Text);
@@ -83,12 +83,12 @@ namespace bweilandt2e1
             int startYear = Convert.ToInt32(input08DTextBox.Text);
             int currentYear = Convert.ToInt32(input08ETextBox.Text);
             result08TextBox.Text = (
-            (thisYTD > lastYTD || empType == "Part time") && startYear < currentYear).ToString();
+            LogicalOperations.q08(thisYTD, lastYTD, empType, startYear, currentYear)).ToString();
 
             //09
             counter = Convert.ToInt32(input09ATextBox.Text);
             years = Convert.ToInt32(input09BTextBox.Text);
-            result09TextBox.Text = (!(counter++ >= years)).ToString();
+            result09TextBox.Text = (LogicalOperations.q09(counter, years)).ToString();
             SideEffect09TextBox.Text = counter.ToString();
 
             //10
@@ -96,7 +96,7 @@ namespace bweilandt2e1
             int b = Convert.ToInt32(input10BTextBox.Text);
             int c = Convert.ToInt32(input10CTextBox.Text);
             int d = Convert.ToInt32(input10DTextBox.Text);
-            result10TextBox.Text = (a > b && (b < c || c < d)).ToString();
+            result10TextBox.Text = (LogicalOperations.q10(a, b, c, d)).ToString();
             // bool v = a > b;
             // bool w = b < c:
             // bool x = c < d;
@@ -105,5 +105,4 @@ namespace bweilandt2e1
             // result10TextBox.Text = z.ToString();
 
         }
-    }
-}
+    } } 
