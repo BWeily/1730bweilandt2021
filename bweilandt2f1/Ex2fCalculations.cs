3d394d48bf6435a9c54e91a3dbfe748e41bf3f35
Bweilandt <bweily643@gmail.com>
2021-10-27 22:44:55 -0500

    completed ex2f

---
+++ bweilandt2f1/Ex2fCalculations.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bweilandt2f1
+{
+    public class Ex2fCalculations
+    {
+        /*
+      This file includes the methods that you must add to the class named Ex2fCalculations
+      The video begins to explain this process at about 6 minutes
+
+      1. Right click on the project for Ex2F and add a new class named Ex2fCalculations
+      2. Made sure the class is public (add the 'public' keyword before the line that starts with 'class')
+      3. The methods that you add to this class are below 
+      */
+
+
+        public static string Calc01(string input)
+        {
+            decimal subtotal = 0m;
+            decimal discountPercent = 0m;
+            subtotal = Decimal.Parse(input);
+            if (subtotal >= 100m)
+                discountPercent = 0.2m;
+            return discountPercent.ToString("n2");
+        }
+
+        public static string Calc02(string input)
+        {
+            decimal subtotal = 0m;
+            decimal discountPercent = 0m;
+            subtotal = Decimal.Parse(input);
+            discountPercent = 0m;
+            string status = "Standard rate: ";
+            if (subtotal >= 100m)
+            {
+                discountPercent = 0.2m;
+                status = "Bulk rate: " + discountPercent.ToString("n2");
+            }
+            return status;
+        }
+
+        public static string Calc03(string input)
+        {
+            decimal subtotal = 0m;
+            decimal discountPercent = 0m;
+            subtotal = Decimal.Parse(input);
+            discountPercent = 0m;
+            if (subtotal >= 100m)
+                discountPercent = 0.2m;
+            else
+                discountPercent = 0.1m;
+            return discountPercent.ToString("n2");
+        }
+
+        public static string Calc04(string input)
+        {
+            decimal subtotal = 0m;
+            decimal discountPercent = 0m;
+            subtotal = Decimal.Parse(input);
+            discountPercent = 0m;
+            if (subtotal >= 100m && subtotal < 200)
+                discountPercent = 0.2m;
+            else if (subtotal >= 200m && subtotal < 300)
+                discountPercent = 0.3m;
+            else if (subtotal >= 300m)
+                discountPercent = 0.4m;
+            else
+                discountPercent = 0.1m;
+            return discountPercent.ToString("n2");
+        }
+
+        public static string Calc05(string input)
+        {
+            decimal subtotal = 0m;
+            decimal discountPercent = 0m;
+            subtotal = Decimal.Parse(input);
+            discountPercent = 0m;
+            if (subtotal >= 300m)
+                discountPercent = 0.4m;
+            else if (subtotal >= 200m)
+                discountPercent = 0.3m;
+            else if (subtotal >= 100m)
+                discountPercent = 0.2m;
+            else
+                discountPercent = 0.1m;
+            return discountPercent.ToString("n2");
+        }
+
+        public static string Calc06(string inputA, string inputB)
+        {
+            decimal subtotal = 0m;
+            decimal discountPercent = 0m;
+            subtotal = Decimal.Parse(inputA);
+            string customerType = inputB;
+            discountPercent = 0m;
+            if (customerType == "R")
+            {
+                if (subtotal >= 100m)
+                    discountPercent = 0.2m;
+                else
+                    discountPercent = 0.1m;
+            }
+            else        // customerType isnt "R"
+                discountPercent = 0.4m;
+            return discountPercent.ToString("n2");
+        }
+
+        public static string Calc07(string input)
+        {
+            decimal ethereum = 0m;
+            if (input != "")
+            {
+                decimal dollars = Decimal.Parse(input);
+                ethereum = 200m * dollars;
+                return ethereum.ToString("n2");
+            }
+            return "Invalid input";
+
+        }
+        public static string Calc08(string inputA, string inputB)
+        {
+            decimal total = 0m;
+            if (inputA != "" && inputB != "")
+                return "Invalid input";
+        }
+
+        public static string Calc09(string inputA, string inputB)
+        {
+            decimal total = 0m;
+            if (inputA != "" && inputB != "")
+                return "Invalid input";
+        }
+
+        public static string Calc10(string inputA, string inputB)
+        {
+            double result = 0.0;
+            if (inputA != "" && inputB != "")
+                return "Invalid input";
+        }
+    }
+}
---
+++ bweilandt2fUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("bweilandt2fUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bweilandt2fUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b6c9ab1a-d635-45d4-89da-d4080b9661ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
